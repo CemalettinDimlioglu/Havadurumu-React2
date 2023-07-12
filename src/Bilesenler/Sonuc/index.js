c9fec16519fc74c5825976bf1dd3aae8f2e22ff9
CMLTTN <cemaldim@gmail.com>
2023-07-12 10:38:51 +0300

    done

--- src/Bilesenler/Sonuc/index.js
+++ src/Bilesenler/Sonuc/index.js
@@ -1,14 +1,19 @@
-
-function index (){
-return (
-  <div className="sonuc">
-    <div className="sehir">Istanbul</div>
-    <div className="detay">
-      <div className="derece">29 °C</div>
-      <div className="statu">Yagmurlu</div>
-    </div>
-  </div>
-);
+function index({ hava }) {
+  return (
+    <>
+    {typeof hava.main != "undefined" && (
+    
+      <div className="sonuc">
+        <div className="sehir">{hava.name},{hava.sys.country}</div>
+        <div className="detay">
+          <div className="derece">{Math.round(hava.main.temp)}°C</div>
+          <div className="statu">{hava.weather[0].description}</div>
+        </div>
+      </div>
+    
+    )}
+    </>
+  );  
 }
 
 export default index;
