eae238277ef3d8a1ad0a3997e694e24d32768af5
CMLTTN <cemaldim@gmail.com>
2023-07-12 10:40:34 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import  {useState} from 'react';
 import "./index.css";
 
 import Arkaplan from "./Bilesenler/Arkaplan";
-import Arama from "./Bilesenler/Arama";
+import Arama from "./Bilesenler/Arama/SearchBar";
 import Sonuc from "./Bilesenler/Sonuc";
 
 function App() {
+
+ const [havaDurumu, setHavaDurumu] = useState({})  
   return (
     <section className="uygulama">
-      <Arkaplan />
+      <Arkaplan arkaplan={havaDurumu.weather && havaDurumu.weather[0].main} />
       <div id="baslik">Hava Durumu</div>
-      <Arama/>
-      <Sonuc/>
+      <Arama setHavaDurumu={setHavaDurumu} />
+      <Sonuc hava={havaDurumu} />
     </section>
   );
 }
